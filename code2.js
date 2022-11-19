1170416318b949b8750e0badd07bb619090b1b2e
pavan-pyth <116789146+pavan-pyth@users.noreply.github.com>
2022-11-19 07:09:59 +0530

    Add files via upload

---
+++ code2.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript HTML Events</h2>
+<button onclick="this.innerHTML=Date()">The time is?</button>
+<button onchange="this.innerHTML=Date()">The time is?</button>
+<button onmouseover="this.innerHTML=Date()">The time is?</button>
+<button onmouseout="this.innerHTML=Date()">The time is?</button
+<button onkeydown="this.innerHTML=Date()">The time is?</button.>
+<button onload="this.innerHTML=Date()">The time is?</button.>
+
+</body>
+</html>
